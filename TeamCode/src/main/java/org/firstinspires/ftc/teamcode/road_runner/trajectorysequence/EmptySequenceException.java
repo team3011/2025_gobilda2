9bd21e0839441979015f4d9dc06ec6eeba222320
pandaLaptop <nrrobotics@gmail.com>
2024-10-15 21:50:50 -0400

    Updated Roadrunner to 1.0. Integrated it with the gobilda pinpoint computer. Currently figuring out how to rotate the ftcdashboard field view to have y be up and down on the screen and the robot heading of 0 be straight forward.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/road_runner/trajectorysequence/EmptySequenceException.java
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.road_runner.trajectorysequence;
-
-
-public class EmptySequenceException extends RuntimeException { }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/tuning/TuningOpModes.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.roadrunner.tuning;
+
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+
+public final class TuningOpModes {
+    public static final Class<?> DRIVE_CLASS = MecanumDrive.class;
+}
