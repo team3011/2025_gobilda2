1aeb5f35325d5199ccd522ab8d142f37ef601e10
pandaLaptop <nrrobotics@gmail.com>
2024-10-29 20:32:31 -0400

    Added parking Autonomous programs. Left and Right are from perspective of drivers.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/RedAutoLeft.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.roadrunner.PinpointDrive;
+@Autonomous
+public class RedAutoLeft extends LinearOpMode {
+
+    public void runOpMode() throws InterruptedException {
+        Pose2d beginPose = new Pose2d(-30.5, -66, Math.PI/2);
+        PinpointDrive drive = new PinpointDrive(hardwareMap, beginPose);
+        waitForStart();
+        Actions.runBlocking(
+                drive.actionBuilder(beginPose)
+                        .strafeTo(new Vector2d(41.5,-64))
+                        .build());
+    }
+}
